9d30aadbbd2ce2a4e1c17bcd914b3d2322caca82
Batuhan Arat <arat_batuhan@windowslive.com>
2024-03-24 18:38:02 +0300

    working on showing decks on landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,48 +5,29 @@ import mongoose from "mongoose";
 import express,{Request,Response} from "express";
 import Sensor from "./models/Sensor";
 import User from "./models/User";
+import cors from "cors";
+import Deck from "./models/Deck";
 
 const PORT = 5001;
 const app = express();
 
+app.use(cors({
+    origin: "*"
+}));
+
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("hello world");
-});
-
-app.get("/hello", (req: Request, res: Response) => {
-    res.send("hello ");
-});
-
-app.post("/sensors", async (req:Request, res: Response) => {
-    const newSensor = new Sensor({
-        temperature: req.body.temperature,
-        humidity: req.body.humidity,
-        ligthIntensity: req.body.ligthIntensity,
-        co2Level: req.body.co2Level
-    })
-    const createdSensor = await newSensor.save();
-    res.json(createdSensor);
-});
-
-app.post("/users", async (req:Request, res: Response) => {
-    const newUser = new User({
-        temperature: req.body.temperature,
-        humidity: req.body.humidity,
-        ligthIntensity: req.body.ligthIntensity,
-        co2Level: req.body.co2Level
-    })
-    const createdUser = await newUser.save();
-    res.json(createdUser);
-});
-
 app.post("/decks", async (req:Request, res: Response) => {
-    const newDeck = new User({
-        title: req.body
+    const newDeck = new Deck({
+        title: req.body.title
     })
     const createdDeck = await newDeck.save();
     res.json(createdDeck);
+});
+
+app.get("/decks", async (req:Request, res: Response) => {
+    const decks = await Deck.find();
+    res.json(decks);
 });
 
 
